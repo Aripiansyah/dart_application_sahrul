065e495941ce5fd5e69ee8f360c86b6b61c24267
Aripriansyah <sahrularipiansyah4@gmail.com>
2025-10-16 15:14:59 +0700

    int: Dart Project

---
+++ bin/dart_application_sahrul.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_sahrul/dart_application_sahrul.dart' as dart_application_sahrul;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_sahrul.calculate()}!');
+}
---
+++ test/dart_application_sahrul_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_sahrul/dart_application_sahrul.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
